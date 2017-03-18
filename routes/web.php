4acb7b3e8c075b242494b4ef708726a28dbba605
Eduardo Salazar <eduardosalazar89@hotmail.es>
2017-03-18 14:19:45 -0600

    [FIX] - Uncomment routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@
 
 Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
-    //Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
-    //Route::get('/cameras', ['as' => 'cameras', 'uses' => 'CameraController@index']);
-    //Route::get('/cameras/create', ['as' => 'cameras.create', 'uses' => 'CameraController@create']);
-    //Route::post('/cameras', ['as' => 'cameras.store', 'uses' => 'CameraController@store']);
+    Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+    Route::get('/cameras', ['as' => 'cameras', 'uses' => 'CameraController@index']);
+    Route::get('/cameras/create', ['as' => 'cameras.create', 'uses' => 'CameraController@create']);
+    Route::post('/cameras', ['as' => 'cameras.store', 'uses' => 'CameraController@store']);
     Route::get('/cameras/{camera}', ['as' => 'cameras.edit', 'uses' => 'CameraController@edit']);
-    //Route::put('/cameras/{camera}', ['as' => 'cameras.update', 'uses' => 'CameraController@update']);
-    //Route::delete('/cameras/{camera}', ['as' => 'cameras.delete', 'uses' => 'CameraController@delete']);
-    //Route::get('/video/proxy/{camera}', ['as' => 'cameras.proxy', 'uses' => 'CameraController@proxy']);
+    Route::put('/cameras/{camera}', ['as' => 'cameras.update', 'uses' => 'CameraController@update']);
+    Route::delete('/cameras/{camera}', ['as' => 'cameras.delete', 'uses' => 'CameraController@delete']);
+    Route::get('/video/proxy/{camera}', ['as' => 'cameras.proxy', 'uses' => 'CameraController@proxy']);
 });
 
