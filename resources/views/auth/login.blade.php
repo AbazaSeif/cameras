117f977464c93f30ad9cf204d72fc094819d382d
Eduardo Salazar <eduardosalazar89@hotmail.es>
2017-04-08 16:37:10 -0500

    [TEST]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>TEST 3</h1>
+    <h1>{{ env('APP_VERSION', 'NOT WORKS') }}</h1>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
